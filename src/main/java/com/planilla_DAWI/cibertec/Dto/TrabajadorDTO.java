abc56991f151d3c66370dc17ff34091de5d66ad8
Vladimir Joya Rojas <121407957+vladimirjoya147@users.noreply.github.com>
2025-08-23 12:39:05 -0500

    dto trabajador

---
+++ src/main/java/com/planilla_DAWI/cibertec/Dto/TrabajadorDTO.java
@@ -0,0 +1,205 @@
+package com.planilla_DAWI.cibertec.Dto;
+
+import java.time.LocalDate;
+
+public class TrabajadorDTO {
+    private Integer idTrabajador;
+    private Integer idTipoDocumento;
+    private String documento;
+    private String nombres;
+    private String apellidoPaterno;
+    private String apellidoMaterno;
+    private Integer idGenero;
+    private Integer idEstadoCivil;
+    private String direccion;
+    private String email;
+    private Integer hijos;
+    private Integer idCargo;
+    private LocalDate fecNacimiento;
+    private LocalDate fecIngreso;
+    private Integer idSituacion;
+    private Integer idSistemaPension;
+    private String desTipoDocumento;
+    private String desGenero;
+    private String desEstadoCivil;
+    private String desCargo;
+    private String desSituacion;
+    private String desSistemaPension;
+
+
+    public Integer getIdTrabajador() {
+        return this.idTrabajador;
+    }
+
+    public Integer getIdTipoDocumento() {
+        return this.idTipoDocumento;
+    }
+
+    public String getDocumento() {
+        return this.documento;
+    }
+
+    public String getNombres() {
+        return this.nombres;
+    }
+
+    public String getApellidoPaterno() {
+        return this.apellidoPaterno;
+    }
+
+    public String getApellidoMaterno() {
+        return this.apellidoMaterno;
+    }
+
+    public Integer getIdGenero() {
+        return this.idGenero;
+    }
+
+    public Integer getIdEstadoCivil() {
+        return this.idEstadoCivil;
+    }
+
+    public String getDireccion() {
+        return this.direccion;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public Integer getHijos() {
+        return this.hijos;
+    }
+
+    public Integer getIdCargo() {
+        return this.idCargo;
+    }
+
+    public LocalDate getFecNacimiento() {
+        return this.fecNacimiento;
+    }
+
+    public LocalDate getFecIngreso() {
+        return this.fecIngreso;
+    }
+
+    public Integer getIdSituacion() {
+        return this.idSituacion;
+    }
+
+    public Integer getIdSistemaPension() {
+        return this.idSistemaPension;
+    }
+
+    public String getDesTipoDocumento() {
+        return this.desTipoDocumento;
+    }
+
+    public String getDesGenero() {
+        return this.desGenero;
+    }
+
+    public String getDesEstadoCivil() {
+        return this.desEstadoCivil;
+    }
+
+    public String getDesCargo() {
+        return this.desCargo;
+    }
+
+    public String getDesSituacion() {
+        return this.desSituacion;
+    }
+
+    public String getDesSistemaPension() {
+        return this.desSistemaPension;
+    }
+
+    public void setIdTrabajador(Integer idTrabajador) {
+        this.idTrabajador = idTrabajador;
+    }
+
+    public void setIdTipoDocumento(Integer idTipoDocumento) {
+        this.idTipoDocumento = idTipoDocumento;
+    }
+
+    public void setDocumento(String documento) {
+        this.documento = documento;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public void setApellidoPaterno(String apellidoPaterno) {
+        this.apellidoPaterno = apellidoPaterno;
+    }
+
+    public void setApellidoMaterno(String apellidoMaterno) {
+        this.apellidoMaterno = apellidoMaterno;
+    }
+
+    public void setIdGenero(Integer idGenero) {
+        this.idGenero = idGenero;
+    }
+
+    public void setIdEstadoCivil(Integer idEstadoCivil) {
+        this.idEstadoCivil = idEstadoCivil;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setHijos(Integer hijos) {
+        this.hijos = hijos;
+    }
+
+    public void setIdCargo(Integer idCargo) {
+        this.idCargo = idCargo;
+    }
+
+    public void setFecNacimiento(LocalDate fecNacimiento) {
+        this.fecNacimiento = fecNacimiento;
+    }
+
+    public void setFecIngreso(LocalDate fecIngreso) {
+        this.fecIngreso = fecIngreso;
+    }
+
+    public void setIdSituacion(Integer idSituacion) {
+        this.idSituacion = idSituacion;
+    }
+
+    public void setIdSistemaPension(Integer idSistemaPension) {
+        this.idSistemaPension = idSistemaPension;
+    }
+
+    public void setDesTipoDocumento(String desTipoDocumento) {
+        this.desTipoDocumento = desTipoDocumento;
+    }
+
+    public void setDesGenero(String desGenero) {
+        this.desGenero = desGenero;
+    }
+
+    public void setDesEstadoCivil(String desEstadoCivil) {
+        this.desEstadoCivil = desEstadoCivil;
+    }
+
+    public void setDesCargo(String desCargo) {
+        this.desCargo = desCargo;
+    }
+
+    public void setDesSituacion(String desSituacion) {
+        this.desSituacion = desSituacion;
+    }
+
+    public void setDesSistemaPension(String desSistemaPension) {
+        this.desSistemaPension = desSistemaPension;
+    }
+}
